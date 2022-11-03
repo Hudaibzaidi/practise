11f09c0cec3870fc0abfedcd4b01e616d6cb3205
Hudaibzaidi <117076704+Hudaibzaidi@users.noreply.github.com>
2022-11-03 15:06:10 +0500

    work is done
    work is done

---
+++ app.js
@@ -0,0 +1,43 @@
+var spec = prompt("Your species?", "human");
+console.log(spec)
+// prompt
+
+
+var x = prompt("Where does the Pope live?");
+ if (x === "Vatican") {
+console.log("correct")}
+// if statment
+
+if (weight > 300 && time < 6) {
+     alert("Come to our tryout!");
+     }
+     else {
+     alert("Come to our cookout!");
+     }
+
+     if (SAT > avg || GPA > 2.5 || sport === "football") {
+         alert("Welcome to Bubba State!");
+         }
+         else {
+         alert("Have you looked into appliance repair?");
+         }
+         //if ... else statment & testing sets and of conditions 
+
+
+         if (c === d) {
+             if (x === y) {
+             g = h;
+             }
+             else if (a === b) {
+             g = h;
+             }
+            else {
+            e = f;
+             }
+            }
+             else {
+             e = f;
+            }
+            // if statment nested
+
+                      
